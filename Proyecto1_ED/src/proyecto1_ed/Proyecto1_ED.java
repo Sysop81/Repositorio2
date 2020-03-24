aa228d46bd1905a33ca1fb6378cb77cf4665d100
sysop <sysop@LAPTOP-T3OLI6DS>
2020-03-24 11:08:37 +0100

    Subir archivos a GitHub, segunda modificacion

--- Proyecto1_ED/src/proyecto1_ed/Proyecto1_ED.java
+++ Proyecto1_ED/src/proyecto1_ed/Proyecto1_ED.java
@@ -8,6 +8,7 @@ package proyecto1_ed;
 /**
  *
  * @author sysop
+ * @version 1.0 24 de Marzo de 2020
  */
 public class Proyecto1_ED {
 
@@ -16,7 +17,7 @@ public class Proyecto1_ED {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        //
+        Persona p1 = new Persona("", "", "", 0, 0, 0);
     }
     
 }
